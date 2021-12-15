66aec64f83bbb6a43d84f64300639b9ca39b31b8
Muhamad Alvin Alzali <68807189+alvinalzali@users.noreply.github.com>
2021-12-15 10:10:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ session_start();
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- custom css-->
     <link rel="stylesheet" href="./Resource/style.css">
     <!-- Add icon library -->
